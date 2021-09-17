919160b6d18008a70dbca72f296799d46e3b4bbc
王明辉 <1213791064@qq.com>
2021-09-17 17:50:19 +0800

    删除本地文件

--- QMLibraryPodManger/QMLibraryPodManger/Example/Pods/Target Support Files/Pods-QMLibraryPodManger_Tests/Pods-QMLibraryPodManger_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_QMLibraryPodManger_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_QMLibraryPodManger_Tests
-@end
--- QMLibraryPodManger/QMLibraryPodManger/Example/Pods/Target Support Files/QMLibraryPodManger/QMLibraryPodManger-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_QMLibraryPodManger : NSObject
-@end
-@implementation PodsDummy_QMLibraryPodManger
-@end
