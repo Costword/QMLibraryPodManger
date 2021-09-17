91529ca49316215e5c9a6898000e991ca8e12ff6
王明辉 <1213791064@qq.com>
2021-09-17 18:42:55 +0800

    编辑文件

---
+++ QMLibraryPodManger/QMBookManger.h
@@ -0,0 +1,17 @@
+//
+//  QMBookManger.h
+//  QMLibraryPodManger
+//
+//  Created by 王明辉 on 2021/9/16.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface QMBookManger : NSObject
+
++(void)QM_RegisterWithAppID:(NSString*)appid;
+@end
+
+NS_ASSUME_NONNULL_END
